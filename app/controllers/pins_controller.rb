5c4f1e7d3907d45d4764dcf001e07510580bf584
github username <github email address>
2020-06-12 19:07:05 -0400

    added index, create action for pins comtroller and strong params for pins

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,2 +1,23 @@
 class PinsController < ApplicationController
+
+    def index
+        pins = Pin.all 
+        render json: PinSerializer.new(pins)
+    end 
+
+    def create 
+        pin = Pin.new(pin_params)
+        if pin.save 
+            render json: PinSerializer.new(pin)
+        else 
+            render json: {errors: pin.errors.full_messages}, status: :unprocessible_entity
+        end 
+    end 
+
+    private
+
+    def pin_params
+        params.require(:pin).permit(:username, :title, :description, :image_url, :brand_id)
+    end 
+
 end
