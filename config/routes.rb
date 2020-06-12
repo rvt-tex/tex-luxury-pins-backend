283db6652f06bffc52ab872c28fefd8d6582f933
github username <github email address>
2020-06-12 19:00:57 -0400

    updated routes rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :comments
-  resources :pins
-  resources :brands
+  resources :comments, only: [:index]
+  resources :pins, only: [:index, :create, :update]
+  resources :brands, only: [:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
