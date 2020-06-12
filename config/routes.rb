8975b2eed35c731e52083579e1028499b107b780
github username <github email address>
2020-06-12 19:54:56 -0400

    updated resources comments with create route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :comments, only: [:index]
+  resources :comments, only: [:index, :create]
   resources :pins, only: [:index, :create, :update]
   resources :brands, only: [:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
