ab9ffb11884b104f43b4d7e2ea2457b81fe49f54
github username <github email address>
2020-06-12 19:04:01 -0400

    add validation for pin model

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,4 +2,6 @@ class Pin < ApplicationRecord
   
   belongs_to :brand
   has_many :comments
+
+  validates_presence_of :username, :title, :image_url, :description, :brand, format: { with: /\A[a-zA-Z]+\Z/ }, message:"Cannot be blank"
 end
